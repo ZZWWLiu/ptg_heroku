a2434374742a9b56ea82f428ba0c221f227ea874
ZZWWLiu <ziwei.liu1020@gmail.com>
2014-06-09 22:31:44 -0700

    update setting for memcache

--- ParkToGo/settings.py
+++ ParkToGo/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import urlparse
+import json
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -116,10 +118,10 @@ STATICFILES_DIRS = (
 CACHES = {
     'default': {
         'BACKEND': 'django_bmemcached.memcached.BMemcached',
-        'LOCATION': os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
+        'LOCATION': "pub-memcache-13202.us-east-1-4.3.ec2.garantiadata.com:13202",
         'OPTIONS': {
-                    'username': os.environ.get('MEMCACHEDCLOUD_USERNAME'),
-                    'password': os.environ.get('MEMCACHEDCLOUD_PASSWORD')
+                    'username': "memcached-app26199140",
+                    'password': "30fLhqCel6JpGiPi"
             }
     }
 }
